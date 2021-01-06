e265e349ffe1f1cec9860362dbc2aabac872cb30
Alessandro Tedeschi <aleted473>
2021-01-06 23:25:31 +0000

    added variables

---
+++ variables.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int a;
+  int b = 3;
+  int c;
+
+  a = 2;
+  c = a + b;
+  printf("Sum of %d and %d is %d\n", a, b, c);
+
+  a = 5;
+  c = 2;
+  printf("A is equal to %d and c is equal to %d ", a, c);
+}
+
+
+  
+	  

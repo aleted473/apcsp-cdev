515778e129b03c66adac03221e1a23edb4fe4bd9
Alessandro Tedeschi <aleted473>
2021-01-07 17:21:51 +0000

    adding size of variables

--- sizeofvariables.c
+++ sizeofvariables.c
@@ -7,6 +7,24 @@ int main()
   // print value and size of an int variable
   printf("int a value: %d and size: %lu bytes\n", a, sizeof(a));
 
-  char c = 'b';
+  char b = 'b';
+
+  printf("char b value: %d and size: %lu bytes\n", b, sizeof(b));
+
+  float c = 'c';
+
+  printf("float c value: %d and size %lu bytes\n", c, sizeof(c));
+
+  double d = 'd';
+
+  printf("double d value: %d and size %lu bytes\n", d, sizeof(d));
+
+  short int e = 321;
+
+  printf("short int e value: %d and size %lu bytes\n", e, sizeof(e));
+
+  long int f = 123;
+
+  printf("long int f value: %d and size %lu bytes\n", f, sizeof(f));
 }
 
